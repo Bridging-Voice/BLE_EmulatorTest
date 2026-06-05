17702c9e292e580bfe31d1be7f73bdec3c77b20c
bobparadiso <bob@bridgingvoice.org>
2026-06-04 21:20:09 -0400

    -update to work with latest iOS Control -no console window

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Globalization;
+﻿using System.Collections;
+using System.Collections.Concurrent;
+using System.Globalization;
 using System.IO.Pipes;
 using System.Text;
 using Windows.Devices.Bluetooth;
@@ -7,12 +9,15 @@ namespace BLE_EmulatorTest;
 
 class Program
 {
-    private static VirtualKeyboard m_virtualKeyboard;
-    private static VirtualMouse m_virtualMouse;
-    private static string m_deviceName;
+    private static VirtualKeyboard? m_virtualKeyboard;
+    private static VirtualMouse? m_virtualMouse;
+    private static string m_deviceName = "NONE";
     private static string VER = "BVEM 1.0.1";
+    private static NamedPipeServerStream? m_pipeIn, m_pipeOut;
+    private static BlockingCollection<string> m_cmds = new BlockingCollection<string>();
+    private static Thread? m_readThread;
 
-    private static async void InitializeVirtualDevices()
+    private static async Task InitializeVirtualDevices()
     {
         try
         {
@@ -44,6 +49,7 @@ class Program
                 var leDevice = await BluetoothLEDevice.FromIdAsync(client.Session.DeviceId.Id);
                 Console.WriteLine("keyboard-subscribed: " + leDevice.Name);
                 m_deviceName = leDevice.Name;
+                WriteString("DEVICE=" + m_deviceName + "\n");
             }
         }
     }
@@ -57,6 +63,7 @@ class Program
                 var leDevice = await BluetoothLEDevice.FromIdAsync(client.Session.DeviceId.Id);
                 Console.WriteLine("mouse-subscribed: " + leDevice.Name);
                 m_deviceName = leDevice.Name;
+                WriteString("DEVICE=" + m_deviceName + "\n");
             }
         }
     }
@@ -91,38 +98,53 @@ class Program
 
         for (int i = 0; i < 4; i++)
         {
-            m_virtualMouse.Press();
+            await m_virtualMouse.Press();
             await Task.Delay(300);
-            m_virtualMouse.Release();
+            await m_virtualMouse.Release();
             await Task.Delay(300);
         }
     }
 
-    private static BinaryReader br = null;
-    private static BinaryWriter bw = null;
-
-    private static string ReadString()
+    private static void ReadStrings()
     {
-        //Console.WriteLine("entering ReadString");
-        var len = (int)br.ReadUInt32();            // Read string length
-        var str = new string(br.ReadChars(len));    // Read string
-        Console.WriteLine("Read: \"{0}\"", str);
-        return str;
+        while (true)
+        {
+            try
+            {
+                if (m_pipeIn is null || !m_pipeIn.IsConnected)
+                {
+                    Thread.Sleep(500);
+                }
+                else
+                {
+                    byte[] buf = new byte[128];
+                    m_pipeIn.Read(buf, 0, buf.Length);
+                    var str = System.Text.Encoding.ASCII.GetString(buf);
+                    m_cmds.Add(str);
+                    Console.WriteLine("Read: \"{0}\"", str);
+                }
+            }
+            catch { Thread.Sleep(500); }
+        }
     }
 
     private static void WriteString(string str)
     {
         //Console.WriteLine("entering WriteString");
-        var buf = Encoding.ASCII.GetBytes(str);     // Get ASCII byte array     
-        bw.Write((uint)buf.Length);                // Write string length
-        bw.Write(buf);                              // Write string
+        var buf = Encoding.ASCII.GetBytes(str);     // Get ASCII byte array
+        if (m_pipeOut is not null && m_pipeOut.IsConnected)
+        {
+            m_pipeOut.Write(buf);
+        }
         Console.WriteLine("Wrote: \"{0}\"", str);
     }
 
     private static async Task run_server()
     {
+        m_readThread = new Thread(ReadStrings);
+        m_readThread.Start();
+
         // Open the named pipe.
-        NamedPipeServerStream server = null;
         bool waitingForConnection = true;
 
         while (true)
@@ -130,54 +152,58 @@ class Program
             if (waitingForConnection)
             {
                 Console.WriteLine("Waiting for connection...");
-                server = new NamedPipeServerStream("BV_BLE_PIPE");
-                server.WaitForConnection();
+                m_pipeIn = new NamedPipeServerStream("BV_BLE_PIPE_IN", PipeDirection.InOut, 1, PipeTransmissionMode.Message, PipeOptions.WriteThrough, 0, 0);
+                m_pipeOut = new NamedPipeServerStream("BV_BLE_PIPE_OUT", PipeDirection.InOut, 1, PipeTransmissionMode.Message, PipeOptions.WriteThrough, 0, 0);
+                m_pipeIn.WaitForConnection();
+                m_pipeOut.WaitForConnection();
 
                 Console.WriteLine("Connected.");
-                br = new BinaryReader(server);
-                bw = new BinaryWriter(server);
                 waitingForConnection = false;
             }
 
             try
             {
-                var str = ReadString().Replace("\n", "").Replace("\r", "");
+                var str = m_cmds.Take().Replace("\n", "").Replace("\r", "").TrimEnd('\0').ToLower();
+                WriteString("\x06\n"); // ACK when we get to actually processing it
 
-                if (str == "AT+BLECURRENTDEVICENAME")
-                {
-                    WriteString(m_deviceName + "\n");
-                }
-                else if (str == "AT+VER")
+                if (str.StartsWith("begin"))
                 {
                     WriteString(VER + "\n");
+                    WriteString("DEVICE=" + m_deviceName + "\n");
                 }
-                else if (str.StartsWith("AT+BLEHIDMOUSEMOVE"))
+                else if (str.StartsWith("ping"))
+                {
+                    WriteString("pong\n");
+                }
+                else if (str.StartsWith("mv"))
                 {
                     var args = str.Split(new char[] { '=' })[1].Split(',');
-                    await m_virtualMouse.Move(int.Parse(args[0]), int.Parse(args[1]), int.Parse(args[2]));
+                    await m_virtualMouse.Move(int.Parse(args[0]), int.Parse(args[1]), 0);
                     WriteString("OK\n");
                 }
-                else if (str.StartsWith("AT+BLEHIDMOUSEBUTTON"))
+                else if (str.StartsWith("mw"))
+                {
+                    var args = str.Split(new char[] { '=' })[1].Split(',');
+                    await m_virtualMouse.Move(0, 0, int.Parse(args[0]));
+                    WriteString("OK\n");
+                }
+                else if (str.StartsWith("mb"))
                 {
                     var args = str.Split(new char[] { '=' })[1].Split(',');
                     if (args[0] == "l")
                     {
-                        if (args[1] == "press")
+                        if (args[1] == "1")
                         {
                             await m_virtualMouse.Press();
                         }
-                        else if (args[1] == "click")
+                        else if (args[1] == "0")
                         {
-                            await m_virtualMouse.Click();
+                            await m_virtualMouse.Release();
                         }
-                    }
-                    else if (args[0] == "0")
-                    {
-                        await m_virtualMouse.Release();
                     }
                     WriteString("OK\n");
                 }
-                else if (str.StartsWith("AT+BLEKEYBOARDCODE"))
+                else if (str.StartsWith("kb"))
                 {
                     var args = str.Split(new char[] { '=' })[1].Split('-');
                     var reportValue = new byte[VirtualKeyboard.c_sizeOfKeyboardReportDataInBytes];
@@ -191,25 +217,33 @@ class Program
                 else
                 {
                     Console.WriteLine("UNKNOWN CMD!!!");
-                    WriteString("OK\n"); //always OK for now
+                    WriteString("ERROR=unknown cmd\n");
                 }
             }
             // When client disconnects
-            catch (EndOfStreamException)
+            catch (System.IO.IOException)
             {
                 Console.WriteLine("Client disconnected.");
-                server.Close();
-                server.Dispose();
+                if (m_pipeIn is not null)
+                {
+                    m_pipeIn.Close();
+                    m_pipeIn.Dispose();
+                    m_pipeIn = null;
+                }
+                if (m_pipeOut is not null)
+                {
+                    m_pipeOut.Close();
+                    m_pipeOut.Dispose();
+                    m_pipeOut = null;
+                }
                 waitingForConnection = true;
             }
         }
     }
 
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
-        InitializeVirtualDevices();
-        //Console.ReadLine();
-        while (true) ;
+        await InitializeVirtualDevices();
     }
 }
